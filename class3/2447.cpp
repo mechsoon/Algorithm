de0aeb4106808d7bf2516ba3c5e3bedeee012b0b
mechsoon <ksw5190@gmail.com>
2025-01-25 16:18:51 +0100

    gold5 recur

--- class3/2447.cpp
+++ class3/2447.cpp
@@ -2,32 +2,35 @@
 #include <vector>
 
 using namespace std;
-
+char arr[3000][3000];
 void paint(int x,int y,int size){
     for(int i=x;i<x+size;i++)
     {
         for(int j=y;j<y+size;j++){
-            if(i==x || i==x+size){
-                cout<<"*";
+            if(i==x || i==x+size-1){
+                arr[i][j]='*';
             }else{
-                if(j==y || j==y+size){
-                    cout<<"*";
-                }
+                if(j==y || j==y+size-1){
+                    arr[i][j]='*';}
+                    
+                // }else{
+                // cout<<" ";}
             }
         }
-        cout<<"\n";
     }
 
 }
 
 void sear(int x,int y,int size){
-    if(size==0){
+    if(size<3){
 
         return;
     }
     paint(x,y,size);
     for(int i=0;i<3;i++)
-    for(int j=0;j<3;i++){
+    for(int j=0;j<3;j++){
+        if(i==1 && j==1)
+            continue;
         sear(x+i*size/3,y+j*size/3,size/3);
     }
 
@@ -37,6 +40,19 @@ void sear(int x,int y,int size){
 int main(){
     int N;
     cin>>N;
-    sear(0,0,N);
+    // paint(0,0,5);
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            arr[i][j] = ' ';
+        }
+    }
+
+    sear(0, 0, N);
+    // sear(0,0,N);
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            cout<<arr[i][j];
+        }cout<<"\n";
+    }
 
 }
